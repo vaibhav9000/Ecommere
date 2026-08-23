5d924e5a48d0514a312e5c6d906870f545270a8b
Vaibhav <sharmavaibhav9000@gmail.com>
2026-08-23 18:53:55 +0530

    Transactions read api's and functions

--- InventoryApp/src/main/java/inventory/app/controller/TransactionController.java
+++ InventoryApp/src/main/java/inventory/app/controller/TransactionController.java
@@ -1,4 +1,32 @@
 package inventory.app.controller;
 
+import inventory.app.model.Transaction;
+import inventory.app.service.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/transactions")
+@AllArgsConstructor
+@PreAuthorize("hasRole('SERVICE_USER')")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @GetMapping
+    public List<Transaction> getTransactions() {
+        return transactionService.getTransactions();
+    }
+
+    @GetMapping("/{id}")
+    public Transaction getTransaction(@PathVariable UUID id) {
+        return transactionService.getTransaction(id);
+    }
 }
--- InventoryApp/src/main/java/inventory/app/service/TransactionService.java
+++ InventoryApp/src/main/java/inventory/app/service/TransactionService.java
@@ -1,4 +1,30 @@
 package inventory.app.service;
 
+import inventory.app.exception.ResourceNotFoundException;
+import inventory.app.model.Transaction;
+import inventory.app.repository.TransactionRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Slf4j
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public List<Transaction> getTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction getTransaction(UUID id) {
+        return transactionRepository.findById(id).orElseThrow(() -> {
+            log.warn("Transaction not found for id: {}", id);
+            return new ResourceNotFoundException("Transaction", id);
+        });
+    }
 }
